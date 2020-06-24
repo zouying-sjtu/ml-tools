ccad817ae977a6bde7bc200c8fd52dd1c6cdffbc
spring <603346577@qq.com>
2020-06-24 11:29:59 +0800

    version-0.0

---
+++ train_val_helper.py
@@ -0,0 +1,200 @@
+from __future__ import print_function, division
+
+import sys
+import time
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.backends.cudnn as cudnn
+
+def adjust_learning_rate_new(epoch, optimizer, LUT):
+    """
+    new learning rate schedule according to RotNet
+    """
+    lr = next((lr for (max_epoch, lr) in LUT if max_epoch > epoch), LUT[-1][1])
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+def adjust_learning_rate(epoch, opt, optimizer):
+    """Sets the learning rate to the initial LR decayed by decay rate every steep step"""
+    steps = np.sum(epoch > np.asarray(opt.lr_decay_epochs))
+    if steps > 0:
+        new_lr = opt.learning_rate * (opt.lr_decay_rate ** steps)
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = new_lr
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
+
+def train_vanilla(epoch, train_loader, model, criterion, optimizer, opt):
+    """vanilla training"""
+    model.train()
+
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+
+    end = time.time()
+    for idx, (input, target) in enumerate(train_loader):
+
+        data_time.update(time.time() - end)
+
+        input = input.float()
+        if torch.cuda.is_available():
+            input = input.cuda()
+            target = target.cuda()
+
+        # ===================forward=====================
+        feats, output = model(input, is_feat=True)
+        loss = criterion(output, target)
+
+        acc1, acc5 = accuracy(output, target, topk=(1, 5))
+        losses.update(loss.item(), input.size(0))
+        top1.update(acc1[0], input.size(0))
+        top5.update(acc5[0], input.size(0))
+
+        # ===================backward=====================
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # ===================meters=====================
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        # tensorboard logger
+        pass
+
+        # print info
+        if idx % opt.print_freq == 0:
+            print('Epoch: [{0}][{1}/{2}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Acc@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                  'Acc@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                   epoch, idx, len(train_loader), batch_time=batch_time,
+                   data_time=data_time, loss=losses, top1=top1, top5=top5))
+            sys.stdout.flush()
+
+    print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
+          .format(top1=top1, top5=top5))
+    return top1.avg, losses.avg
+
+def validate(val_loader, model, criterion, opt):
+    """validation"""
+    batch_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+
+    # switch to evaluate mode
+    model.eval()
+
+    with torch.no_grad():
+        end = time.time()
+        for idx, (input, target) in enumerate(val_loader):
+
+            input = input.float()
+            if torch.cuda.is_available():
+                input = input.cuda()
+                target = target.cuda()
+
+            # compute output
+            feats, output = model(input, is_feat=True)
+            loss = criterion(output, target)
+
+            # measure accuracy and record loss
+            acc1, acc5 = accuracy(output, target, topk=(1, 5))
+            losses.update(loss.item(), input.size(0))
+            top1.update(acc1[0], input.size(0))
+            top5.update(acc5[0], input.size(0))
+
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+            if idx % opt.print_freq == 0:
+                print('Test: [{0}/{1}]\t'
+                      'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                      'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                      'Acc@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                      'Acc@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                       idx, len(val_loader), batch_time=batch_time, loss=losses,
+                       top1=top1, top5=top5))
+
+        print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
+              .format(top1=top1, top5=top5))
+    return top1.avg, top5.avg, losses.avg
+
+def collect_feat(train_loader, model, feat_layer = 3):
+    '''
+    for resnet34
+        torch.Size([16, 16, 32, 32])
+        torch.Size([16, 16, 32, 32])
+        torch.Size([16, 32, 16, 16])
+        torch.Size([16, 64, 8, 8])
+        torch.Size([16, 64])
+    '''
+    # switch to evaluate mode
+    model.eval()
+
+    feat_all = None
+    with torch.no_grad():
+        for idx, (input, target) in enumerate(train_loader):
+            input = input.float()
+            if torch.cuda.is_available():
+                input = input.cuda()
+                target = target.cuda()
+            # compute output
+            feats, output = model(input, is_feat=True, preact=True)
+            feat = feats[feat_layer].detach()
+            
+            feat = feat.permute(0, 2, 3, 1).contiguous()
+            dim = feat.shape[-1]
+            feat = feat.view(-1, dim)
+            if feat_all is None:
+                feat_all = feat
+            else:
+                feat_all = torch.cat([feat_all, feat], dim=0)
+            if idx % 100 == 0:
+                print('collect: [{0}/{1}]'.format(idx, len(train_loader)))
+    return feat_all
+
+if __name__ == '__main__':
+
+    pass
